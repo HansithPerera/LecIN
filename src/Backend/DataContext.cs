59bd5d13d43ddea519675d512a76ae38c3f8f8a7
jonOriginal <jonathan-knox@outlook.com>
2025-08-26 15:23:03 +1200

    Add appsettings.Dev.json to .gitignore to exclude development configuration files

--- src/Backend/DataContext.cs
+++ src/Backend/DataContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend;
 
 public class DataContext: DbContext
 {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
     
+    public DbSet<Student> Students => Set<Student>();
 }
---
+++ src/Backend/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentController (IDbContextFactory<DataContext> ctxFactory):  ControllerBase
+{
+
+    #region Get
+    
+    [HttpGet("profile")]
+    [ProducesResponseType(typeof(Student), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfile()
+    {
+        var ctx = await ctxFactory.CreateDbContextAsync();
+        var student = await ctx.Students.FirstOrDefaultAsync();
+        
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("classes")]
+    [ProducesResponseType(typeof(List<Class>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetClasses()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("courses")]
+    [ProducesResponseType(typeof(List<Course>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetCourses()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("courses/{courseCode}/teachers")]
+    [ProducesResponseType(typeof(List<Teacher>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetTeachersForCourse(string courseCode)
+    {
+        throw new NotImplementedException();
+    }
+    
+    #endregion
+    
+    #region Post
+    
+    [HttpPost("checkin")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult CheckInToCourse([FromBody] string courseCode)
+    {
+        throw new NotImplementedException();
+    }
+    
+    #endregion
+}
