9895cae7a0a4e3614b6dc181d95a0386f5a8786c
jonOriginal <jonathan-knox@outlook.com>
2025-08-29 21:32:46 +1200

    Add AdminController for retrieving teachers and update OpenApi.json for new endpoint

---
+++ src/Backend/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController(IDbContextFactory<DataContext> ctxFactory) : ControllerBase
+{
+    [HttpGet("teachers")]
+    [ProducesResponseType(typeof(List<Teacher>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAllTeachers()
+    {
+        await using var ctx = await ctxFactory.CreateDbContextAsync();
+        var teachers = await ctx.Teachers.ToListAsync();
+        return Ok(teachers);
+    }
+}
--- src/Backend/Controllers/StudentController.cs
+++ src/Backend/Controllers/StudentController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Backend;
+namespace Backend.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -10,16 +10,6 @@ public class StudentController (IDbContextFactory<DataContext> ctxFactory):  Con
 {
 
     #region Get
-    
-    [HttpGet("teachers")]
-    [ProducesResponseType(typeof(List<Teacher>), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetAllTeachers()
-    {
-        await using var ctx = await ctxFactory.CreateDbContextAsync();
-        var teachers = await ctx.Teachers.ToListAsync();
-        return Ok(teachers);
-    }
     
     [HttpGet("profile")]
     [ProducesResponseType(typeof(Student), StatusCodes.Status200OK)]
