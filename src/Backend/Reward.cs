79b48fd227258d2d090963c5ed1ab869b1523011
divarsh <darshds1844@gmail.com>
2025-09-02 14:18:35 +1200

    Reward class

---
+++ src/Backend/Reward.cs
@@ -0,0 +1,10 @@
+﻿namespace LecIN_System.Models
+{
+    public class Reward
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public string RewardType { get; set; } // e.g., "5 Day Streak", "Perfect Attendance"
+        public DateTime DateAwarded { get; set; }
+    }
+}
