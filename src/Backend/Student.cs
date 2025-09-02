2385bb966b3c07c4e99646082d90dfab910bc65c
divarsh <darshds1844@gmail.com>
2025-09-02 14:16:24 +1200

    Student class

---
+++ src/Backend/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LecIN_System.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentId { get; set; }
+        public string FacialDataReference { get; set; }
+        public List<Attendance> AttendanceHistory { get; set; } = new List<Attendance>();
+    }
+}
