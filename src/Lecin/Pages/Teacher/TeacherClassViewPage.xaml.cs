758d1a8157aadf1c074ff58df8dd0503f0046582
jonOriginal <jonathan-knox@outlook.com>
2025-09-26 22:02:50 +1200

    feat: add attendance marking functionality and display enrolled students in TeacherClassViewPage

--- src/Lecin/Pages/Teacher/TeacherClassViewPage.xaml.cs
+++ src/Lecin/Pages/Teacher/TeacherClassViewPage.xaml.cs
@@ -26,4 +26,33 @@ public partial class TeacherClassViewPage : BaseContentPage
             await DisplayAlert("Error", ex.Message, "OK");
         }
     }
+
+    private async void MarkPresent(object? sender, EventArgs e)
+    {
+        try
+        {
+            if (BindingContext is not TeacherClassViewPageModel vm) return;
+            if (sender is not Button { BindingContext: SupabaseShared.Models.Student student }) return;
+            await vm.SetAttendance(student.Id, "Present");
+        }
+        catch (Exception exception)
+        {
+            await DisplayAlert("Error", exception.Message, "OK");
+        }
+        
+    }
+
+    private async void MarkLate(object? sender, EventArgs e)
+    {
+        try
+        { 
+            if (BindingContext is not TeacherClassViewPageModel vm) return;
+            if (sender is not Button { BindingContext: SupabaseShared.Models.Student student }) return;
+            await vm.SetAttendance(student.Id, "Late");
+        }
+        catch (Exception exception)
+        {
+            await DisplayAlert("Error", exception.Message, "OK");
+        }
+    }
 }
