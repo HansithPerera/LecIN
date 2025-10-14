e02f05134068fc59f31c0b069c8778bd5bc09757
Hansith Perera <hansith321@gmail.com>
2025-10-15 03:00:01 +1300

    Feat-checkin page updates

--- src/Lecin/PageModels/CheckInPageModel.cs
+++ src/Lecin/PageModels/CheckInPageModel.cs
@@ -1,42 +1,21 @@
-using System.Windows.Input;
-using CommunityToolkit.Maui.Core;
-using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.Devices.Sensors;
 
 namespace Lecin.PageModels;
 
 public partial class CheckInPageModel : BasePageModel
 {
-    private CameraView? _cameraView;
-
     [ObservableProperty] private ImageSource? _photoPreview;
     [ObservableProperty] private bool _hasCapturedPhoto;
     [ObservableProperty] private DateTime _capturedAt = DateTime.Now;
     [ObservableProperty] private Location? _location;
     [ObservableProperty] private string _locationDisplay = "";
 
-    public IAsyncRelayCommand CapturePhotoCommand { get; }
-    public IAsyncRelayCommand ConfirmCommand { get; }
-
-    public bool CanCapture => _cameraView != null && !_hasCapturedPhoto;
-    public bool CanConfirm => _hasCapturedPhoto && _location != null;
-
     public CheckInPageModel()
     {
-        CapturePhotoCommand = new AsyncRelayCommand(CapturePhoto, () => CanCapture);
-        ConfirmCommand = new AsyncRelayCommand(Confirm, () => CanConfirm);
     }
 
-    public void InitializeCameraView(CameraView cameraView)
-    {
-        _cameraView = cameraView;
-        _cameraView.MediaCaptured -= OnMediaCaptured;
-        _cameraView.MediaCaptured += OnMediaCaptured;
-        _ = PrepareAsync();
-        NotifyCanExec();
-    }
+    public void OnPageAppearing() => _ = PrepareAsync();
 
     private async Task PrepareAsync()
     {
@@ -65,39 +44,27 @@ public partial class CheckInPageModel : BasePageModel
         {
             LocationDisplay = "Unavailable";
         }
-        finally
-        {
-            NotifyCanExec();
-        }
+        finally { }
     }
 
-    private Task CapturePhoto()
+    public void OnCaptureRequested()
     {
         CapturedAt = DateTime.Now;
-        _cameraView?.CaptureImage();
-        return Task.CompletedTask;
     }
 
-    private void OnMediaCaptured(object? sender, MediaCapturedEventArgs e)
+    public void OnMediaCaptured(byte[] imageData)
     {
-        PhotoPreview = ImageSource.FromStream(() => new MemoryStream(e.ImageData));
+        PhotoPreview = ImageSource.FromStream(() => new MemoryStream(imageData));
         HasCapturedPhoto = true;
-        NotifyCanExec();
     }
 
-    private async Task Confirm()
+    public async Task Confirm()
     {
         // Placeholder: Here you would send photo bytes + time + location to backend
         await Application.Current!.MainPage!.DisplayAlert("Checked In",
             $"Time: {CapturedAt:HH:mm:ss}\nLocation: {LocationDisplay}", "OK");
 
         await Shell.Current.GoToAsync("..");
-    }
-
-    private void NotifyCanExec()
-    {
-        CapturePhotoCommand.NotifyCanExecuteChanged();
-        ConfirmCommand.NotifyCanExecuteChanged();
     }
 }
 
--- src/Lecin/Pages/CheckInPage.xaml.cs
+++ src/Lecin/Pages/CheckInPage.xaml.cs
@@ -1,14 +1,30 @@
-using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Maui.Core;
 
 namespace Lecin.Pages;
 
-public partial class CheckInPage : ContentPage
+ public partial class CheckInPage : ContentPage
 {
-    public CheckInPage(PageModels.CheckInPageModel vm)
+    public CheckInPage()
     {
         InitializeComponent();
+        var vm = new PageModels.CheckInPageModel();
         BindingContext = vm;
-        vm.InitializeCameraView(Camera);
+        vm.OnPageAppearing();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+
+    private void CapturePhotoButton_Clicked(object? sender, EventArgs e)
+    {
+        if (BindingContext is PageModels.CheckInPageModel vm) vm.OnCaptureRequested();
+    }
+
+    private async void ConfirmButton_Clicked(object? sender, EventArgs e)
+    {
+        if (BindingContext is PageModels.CheckInPageModel vm) await vm.Confirm();
     }
 }
 
