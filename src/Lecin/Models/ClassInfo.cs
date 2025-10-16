ce4fc52901107d9ebef833b46b209e6c582c6877
divarsh <darshds1844@gmail.com>
2025-10-16 15:11:07 +1300

    Added models and views for the frontend

---
+++ src/Lecin/Models/ClassInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecin.Models { }
+public class ClassInfo
+{
+    public string ClassName { get; set; }
+    public string TeacherName { get; set; }
+    public DateTime StartTime { get; set; }
+    public string TeacherId { get; set; }
+}
+
