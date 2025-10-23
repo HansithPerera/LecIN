7a1a88748ca37768c3fbe54624788d8678ca493a
divarsh <darshds1844@gmail.com>
2025-10-23 21:29:19 +1300

    View for treacher profile

---
+++ src/TeacherProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Lecin.Views
+{
+    public partial class TeacherProfilePage : ContentPage
+    {
+        public Teacher TeacherDetails { get; set; }
+
+        public TeacherProfilePage(Teacher teacher)
+        {
+            InitializeComponent();
+            TeacherDetails = teacher;
+            BindingContext = TeacherDetails;
+        }
+    }
+
+    public class Teacher
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Department { get; set; }
+        public string OfficeLocation { get; set; }
+    }
+}
