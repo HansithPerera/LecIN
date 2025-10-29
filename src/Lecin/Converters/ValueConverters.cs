b6c95c8c70e220766da967680ad4f9bf5bff2ab9
jonOriginal <jonathan-knox@outlook.com>
2025-10-29 19:22:31 +1300

    refactor: clean up value converters and update namespaces in XAML

--- src/Lecin/Converters/ValueConverters.cs
+++ src/Lecin/Converters/ValueConverters.cs
@@ -88,38 +88,21 @@ public class InvertedBoolConverter : IValueConverter
         if (value is bool boolValue) return !boolValue;
         return false;
     }
-    
-    public class HasPhotoToButtonTextConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (value is bool hasPhoto)
-            {
-                return hasPhoto ? "Recapture" : "Capture Photo";
-            }
-            return "Capture Photo";
-        }
+}
 
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+public class HasPhotoToButtonTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool hasPhoto)
         {
-            throw new NotImplementedException();
+            return hasPhoto ? "Recapture" : "Capture Photo";
         }
+        return "Capture Photo";
     }
 
-    public class HasPhotoToButtonTextConverter : IValueConverter
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (value is bool hasPhoto)
-            {
-                return hasPhoto ? "Recapture" : "Capture Photo";
-            }
-            return "Capture Photo";
-        }
-
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
     }
 }
--- src/Lecin/Models/ClassInfo.cs
+++ src/Lecin/Models/ClassInfo.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lecin.Models;
 
-namespace Lecin.Models { }
 public class ClassInfo
 {
     public string ClassName { get; set; }
@@ -12,4 +7,3 @@ public class ClassInfo
     public DateTime StartTime { get; set; }
     public string TeacherId { get; set; }
 }
-
